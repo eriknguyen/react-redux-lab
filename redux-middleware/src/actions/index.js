148370dafc75ff529200bbf216a636f2f748d7bd
Erik Nguyen <namkhanh164@gmail.com>
2017-07-11 12:49:56 +0800

    add weather api, setup axios for fetching, setup redux-promise as redux middleware

--- redux-middleware/src/actions/index.js
+++ redux-middleware/src/actions/index.js
@@ -1,2 +1,17 @@
-const API_KEY = '7238b7afd8d29d89bb6431f7d9de9322';
+import axios from 'axios';
 
+const API_KEY = '7238b7afd8d29d89bb6431f7d9de9322';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// action creator: function return action
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-middleware';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -11,7 +11,7 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<App />
+	</Provider>
+	, document.querySelector('.container'));
