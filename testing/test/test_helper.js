855b51c633c51977d713c98ff214225df6791d44
Erik Nguyen <namkhanh164@gmail.com>
2018-03-03 00:55:51 +0800

    setup test env: final setup for chai-jquery library

--- testing/test/test_helper.js
+++ testing/test/test_helper.js
@@ -3,13 +3,16 @@ import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
 
-/* Setup testing environment to run like a browser in the command line */
+/** 
+ * 1. Setup testing environment to run like a browser in the command line 
+ */
 
 // check out jsdom document
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
@@ -21,14 +24,14 @@ const $ = jquery(global.window);
 
 
 /**
- * Build `renderComponent` helper that render given React component
+ * 2. Build `renderComponent` helper that render given React component
  */
 function renderComponent(ComponentClass, props, state) {
   // create an instance of ComponentClass and render it into document with React test-utils
   const componentInstance = TestUtils.renderIntoDocument(
     // wrap the component with react-redux Provider
     <Provider store={createStore(reducers, state)}>
-      <ComponentClass { ...props } />
+      <ComponentClass {...props} />
     </Provider>
   );
   // return an actual DOM reference to that instancce wrapped by jQuery so chai-jquery can be used to test
@@ -36,10 +39,25 @@ function renderComponent(ComponentClass, props, state) {
 }
 
 
-// Build helpers for simulating events
+/**
+ * 3. Build helpers for simulating events
+ */
+// add a new function to jQuery
+$.fn.simulate = function(eventName, value) {
+  if (value) {
+    this.val(value);
+  }
+  // instead of TestUtils.Simulate.change(), need to refer to the event method by string reference
+  // this is the jQuery reference of DOM element, eg. $('div') and can be an array => use this[0] to make sure it's the first one
+  TestUtils.Simulate[eventName](this[0]);
+}
 
 
-// Setup `chai-jquery` for testing DOM element
+/**
+ * 4. Setup `chai-jquery` for testing DOM element
+ * Refer to chai-jquery documentation
+ */
+chaiJquery(chai, chai.util, $);
 
 
 export { renderComponent, expect };
